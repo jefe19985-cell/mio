1ffad31c37cc42c55022187953ba62b9576d5350
javier <jefe19985@gmail.com>
2026-08-06 09:38:56 -0500

    bueno

--- lalala.java
+++ lalala.java
@@ -1,2 +1,12 @@
+import javax.swing.*;
+
 public class lalala {
+    public static void main (String[] args){
+        String num1=JOptionPane.showInputDialog("introduce numero");
+        double num20=Double.parseDouble(num1);
+        System.out.print(num20);
+        System.out.printf("%1.2f",Math.sqrt(num20));
+
+
+    }
 }
