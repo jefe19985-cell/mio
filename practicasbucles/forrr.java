14f70e13d26ab7278f4eb2a000326902e7103a4c
javier <jefe19985@gmail.com>
2026-08-11 11:48:10 -0500

    lalala8

--- practicasbucles/forrr.java
+++ practicasbucles/forrr.java
@@ -1,8 +1,20 @@
+import javax.swing.*;
+
 public class forrr {
 }
 public static void main (String[] args){
-    for ( int i= 0;i<10;i++){
-        System.out.println("an");
+    boolean arroba= false;
+    String email= JOptionPane.showInputDialog("introduce mail");
+    for (int i=0; i<email.length();i++){
+        if (email.charAt(i)== '@'){
+             arroba=true;
+        }
     }
+    if (arroba==true){
+        System.out.println("es correcto");
+    } else if (arroba==false) {
+        System.out.println("incorrecto");
+    }
+
     //recorrer cadenas de textos.
 }
