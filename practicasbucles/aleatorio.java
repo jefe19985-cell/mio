7aed8caaeaed48c9bd977f0a9b36ae856d305b67
javier <jefe19985@gmail.com>
2026-08-11 10:34:42 -0500

    lalala5

--- practicasbucles/aleatorio.java
+++ practicasbucles/aleatorio.java
@@ -1,17 +1,26 @@
 package practicasbucles;
 
 import javax.swing.*;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class aleatorio {
     public static void  main(String[] args){
         int aleatorio=(int)(Math.random()*100);
-        System.out.println(aleatorio);
-        aleatorio=Integer.parseInt(JOptionPane.showInputDialog(aleatorio));
-        while (aleatorio==aleatorio) {
-            System.out.println("correcto");
-            aleatorio++;
-         break;
-        }
+        Scanner sacaner=new Scanner(System.in);
+        int num=0;
+        int attem=0;
+        while (num!=aleatorio){
+            System.out.println("introduce nombre porfavor");
+            num=sacaner.nextInt();
+            if (aleatorio<num){
+                System.out.println("mas bajo");
 
+            } else if (aleatorio>num) {
+                System.out.println("mas alto");
+            }
+
+        }
+        System.out.println("correcto");
     }
 }
