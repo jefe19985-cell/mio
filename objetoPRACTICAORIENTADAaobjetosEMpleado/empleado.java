a71ff0353dd7dbe497e24d08eeefe777f493b677
javier <jefe19985@gmail.com>
2026-08-26 11:32:27 -0500

    practicando arrays, y bucles for meoradoes objeto empleado

---
+++ objetoPRACTICAORIENTADAaobjetosEMpleado/empleado.java
@@ -0,0 +1,45 @@
+package objetoPRACTICAORIENTADAaobjetosEMpleado;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class empleado {
+    public empleado(String nombr, double sueld, int año,int mes, int dia) {
+nombre=nombr;
+sueldo=sueld;
+GregorianCalendar calendario=new GregorianCalendar(año,mes-1,dia);
+altaContrato=calendario.getTime();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public Date getAltaContrato() {
+        return altaContrato;
+    }
+
+    public void setAltaContrato(Date altaContrato) {
+        this.altaContrato = altaContrato;
+    }
+   public void subeSueldo(double porcentaje){
+        double aument=sueldo*porcentaje/100;
+        sueldo+=aument;
+
+   }
+    private String nombre;
+    private double sueldo;
+    private Date altaContrato;
+}
---
+++ objetoPRACTICAORIENTADAaobjetosEMpleado/usoEmpleado.java
@@ -0,0 +1,31 @@
+package objetoPRACTICAORIENTADAaobjetosEMpleado;
+
+public class usoEmpleado {
+    public static void main (String []args){
+/* empleado Empleado=new empleado("javier",85000,1990,12,1);
+        empleado Empleado2=new empleado("javo",95000,1995,6,2);
+        empleado Empleado3=new empleado("martin",105000,2002,3,15);
+        Empleado.subeSueldo(5);
+        Empleado.subeSueldo(5);
+        Empleado.subeSueldo(5);
+        System.out.println(Empleado.getNombre()+ "   "+"seldo" +Empleado.getSueldo()+" fech alta:"+Empleado.getAltaContrato());*/
+        empleado [] Empleado= new empleado[3];
+                 Empleado[0]=new empleado("javier",85000,1990,12,1);
+                 Empleado[1]=new empleado("javo",95000,1995,6,2);
+                 Empleado[2]=new empleado("martin",105000,2002,3,15);
+
+                 //for (int i=0;i<3;i++){
+//Empleado[i].subeSueldo(5);
+               //  }
+
+        for (empleado e:Empleado ){
+            e.subeSueldo(5);
+        }
+        for (int i=0;i<3;i++){
+            System.out.println(Empleado[i].getNombre()+Empleado[i].getSueldo()+Empleado[i].getAltaContrato());
+        }
+
+    }
+
+
+}
